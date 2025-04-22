dc462c340ceffac071cc74e660a512ae70863381
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-04-22 12:15:40 +0530

    update

--- React_CURD_Axios/src/App.jsx
+++ React_CURD_Axios/src/App.jsx
@@ -1,9 +1,8 @@
 import { Posts } from "./Component/Posts";
 
 
-const App =() =>{
-
-  return <Posts/>
-} ;
+const App = () => {
+  return <Posts />;
+};
 
 export default App;  
--- React_CURD_Axios/src/Component/Posts.jsx
+++ React_CURD_Axios/src/Component/Posts.jsx
@@ -1,16 +1,35 @@
-import { useEffect } from "react";
+import { useEffect ,useState} from "react";
 import { getPost } from "../api/PostApi";
 
 export const Posts = () => {
-    
+  
+  const [data, setData] = useState([]);
+  
   const getPostData = async () => {
     const res = await getPost();
     console.log(res.data);
+    setData(res.data);
   };
 
   useEffect(() => {
     getPostData();
   }, []);
 
-  return <h1>Hello Post Page</h1>;
+  return (
+    <section className="section-post">
+      <ul>
+        {data.map((curElem) => {
+          const { id, title, body } = curElem;
+          return (
+            <li key={id}>
+              <p>Title : {title}</p>
+              <p>Body : {body}</p>
+              <button className="bg-green-400">Edit</button>
+              <button className="bg-orange-400">Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 };
